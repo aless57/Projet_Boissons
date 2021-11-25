a9dd9965bd0b29f0a7bdac08f7b9a3a4b933acac
Alessi <alessi.demange2@etu.univ-lorraine.fr>
2021-11-25 10:33:27 +0100

    Hierarchie test

--- src/controls/ControleurAccueil.php
+++ src/controls/ControleurAccueil.php
@@ -2,9 +2,10 @@
 
 
 namespace boissons\controls;
-
+include 'src/models/Donnees.inc.php';
 use Psr\Http\Message\ResponseInterface as Response;
 use Psr\Http\Message\ServerRequestInterface as Request;
+
 
 use boissons\views\VueAccueil;
 
