16b93d9d4e90368f1390c58f98dd14ee72aaf182
Moitrier Arthur <62136130+JoCk3rZ@users.noreply.github.com>
2021-12-02 11:14:48 +0100

    bdd

--- install.php
+++ install.php
@@ -1,5 +1,31 @@
 <?php
 
-include 'src/models/Donnees.inc.php';
+include('Donnees.inc.php');
 
-var_dump($Recettes[0]);
+$mysqli = mysqli_connect('localhost', 'root', '') or die("Erreur de connexion");
+mysqli_query($mysqli,"USE Projet_Boissons") or die("USE Projet_Boissons : ".mysqli_error($mysqli));
+
+foreach ($Hierarchie as $key => $value) {
+    $Sql="INSERT INTO Hierarchie VALUE (\"$key\")";
+    $res = mysqli_query($mysqli,$Sql) or die($Sql . " : " . mysqli_error($mysqli));
+    if(isset($value['sous-categorie'])){
+        foreach ($value['sous-categorie'] as $sous){
+            $Sql="INSERT INTO  `sous-categorie` (`nomH`, `nomSC`)  VALUES(\"$key\",\"$sous\")";
+            $res = mysqli_query($mysqli,$Sql) or die($Sql . " : " . mysqli_error($mysqli));
+        }
+    }
+
+    if(isset($value['super-categorie'])){
+        foreach ($value['super-categorie'] as $sup){
+            $Sql="INSERT INTO  `super-categorie` (`nomH`, `nomSC`)  VALUES(\"$key\",\"$sup\")";
+            $res = mysqli_query($mysqli,$Sql) or die($Sql . " : " . mysqli_error($mysqli));
+        }
+    }
+
+}
+
+foreach ($Recettes as $key => $value){
+
+}
+
+mysqli_close($mysqli);
