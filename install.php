f8a5ce6b942ec72d6f81a94b3a0fd1f73cef9c3a
JoCk3rZ <arthurmoit@gmail.com>
2021-12-03 16:17:56 +0100

    ajout recette

--- install.php
+++ install.php
@@ -14,18 +14,26 @@ foreach ($Hierarchie as $key => $value) {
             $res = mysqli_query($mysqli,$Sql) or die($Sql . " : " . mysqli_error($mysqli));
         }
     }
-
     if(isset($value['super-categorie'])){
         foreach ($value['super-categorie'] as $sup){
             $Sql="INSERT INTO  `super-categorie` (`nomH`, `nomSC`)  VALUES(\"$key\",\"$sup\")";
             $res = mysqli_query($mysqli,$Sql) or die($Sql . " : " . mysqli_error($mysqli));
         }
     }
-
 }
 
-foreach ($Recettes as $key => $value){
-
+foreach ($Recettes as $value){
+    $titre = $value['titre'];
+    $ing = $value['ingredients'];
+    $prepa = $value['preparation'];
+    $Sql="INSERT INTO  `recettes` (`titre`, `ingredients`, `preparation`)  VALUES(\"$titre\",\"$ing\",\"$prepa\")";
+    $res = mysqli_query($mysqli,$Sql) or die($Sql . " : " . mysqli_error($mysqli));
+    $Sql="SELECT id from recettes where titre like \"$titre\"";
+    $id = mysqli_query($mysqli,$Sql) or die($Sql . " : " . mysqli_error($mysqli));
+    foreach ($value['index'] as $i){
+        $Sql="INSERT INTO  `index` (`idRecette`, `nom`)  VALUES(\"$id\",\"$i\")";
+        $res = mysqli_query($mysqli,$Sql) or die($Sql . " : " . mysqli_error($mysqli));
+    }
 }
 
 mysqli_close($mysqli);
