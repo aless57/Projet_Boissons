f952d27829add18ddf14a08a0548d303ed899453
JoCk3rZ <arthurmoit@gmail.com>
2021-12-20 15:34:00 +0100

    pour creer la table

---
+++ projet_boissons.sql
@@ -0,0 +1,193 @@
+-- phpMyAdmin SQL Dump
+-- version 5.1.0
+-- https://www.phpmyadmin.net/
+--
+-- Hôte : 127.0.0.1
+-- Généré le : lun. 20 déc. 2021 à 15:21
+-- Version du serveur :  10.4.18-MariaDB
+-- Version de PHP : 8.0.3
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Base de données : `projet_boissons`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Structure de la table `contient`
+--
+
+CREATE TABLE `contient` (
+  `idPanier` int(10) NOT NULL,
+  `idRecette` int(10) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+-- --------------------------------------------------------
+
+--
+-- Structure de la table `hierarchie`
+--
+
+CREATE TABLE `hierarchie` (
+  `nom` varchar(30) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+-- --------------------------------------------------------
+
+--
+-- Structure de la table `index`
+--
+
+CREATE TABLE `index` (
+  `idIndex` int(10) NOT NULL,
+  `idRecette` int(10) NOT NULL,
+  `nom` varchar(100) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+-- --------------------------------------------------------
+
+--
+-- Structure de la table `panier`
+--
+
+CREATE TABLE `panier` (
+  `id` int(10) NOT NULL,
+  `login` varchar(20) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+-- --------------------------------------------------------
+
+--
+-- Structure de la table `recettes`
+--
+
+CREATE TABLE `recettes` (
+  `id` int(10) NOT NULL,
+  `titre` varchar(200) NOT NULL,
+  `ingredients` varchar(300) NOT NULL,
+  `preparation` varchar(300) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+-- --------------------------------------------------------
+
+--
+-- Structure de la table `sous-categorie`
+--
+
+CREATE TABLE `sous-categorie` (
+  `nomH` varchar(30) NOT NULL,
+  `nomSC` varchar(30) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+-- --------------------------------------------------------
+
+--
+-- Structure de la table `super-categorie`
+--
+
+CREATE TABLE `super-categorie` (
+  `nomH` varchar(30) NOT NULL,
+  `nomSC` varchar(30) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+-- --------------------------------------------------------
+
+--
+-- Structure de la table `utilisateur`
+--
+
+CREATE TABLE `utilisateur` (
+  `nom` varchar(20) NOT NULL,
+  `prenom` varchar(20) NOT NULL,
+  `login` varchar(20) NOT NULL,
+  `mdp` varchar(20) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Index pour les tables déchargées
+--
+
+--
+-- Index pour la table `contient`
+--
+ALTER TABLE `contient`
+  ADD PRIMARY KEY (`idPanier`,`idRecette`);
+
+--
+-- Index pour la table `hierarchie`
+--
+ALTER TABLE `hierarchie`
+  ADD PRIMARY KEY (`nom`);
+
+--
+-- Index pour la table `index`
+--
+ALTER TABLE `index`
+  ADD PRIMARY KEY (`idIndex`);
+
+--
+-- Index pour la table `panier`
+--
+ALTER TABLE `panier`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Index pour la table `recettes`
+--
+ALTER TABLE `recettes`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Index pour la table `sous-categorie`
+--
+ALTER TABLE `sous-categorie`
+  ADD PRIMARY KEY (`nomH`,`nomSC`);
+
+--
+-- Index pour la table `super-categorie`
+--
+ALTER TABLE `super-categorie`
+  ADD PRIMARY KEY (`nomH`,`nomSC`);
+
+--
+-- Index pour la table `utilisateur`
+--
+ALTER TABLE `utilisateur`
+  ADD PRIMARY KEY (`login`);
+
+--
+-- AUTO_INCREMENT pour les tables déchargées
+--
+
+--
+-- AUTO_INCREMENT pour la table `index`
+--
+ALTER TABLE `index`
+  MODIFY `idIndex` int(10) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT pour la table `panier`
+--
+ALTER TABLE `panier`
+  MODIFY `id` int(10) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT pour la table `recettes`
+--
+ALTER TABLE `recettes`
+  MODIFY `id` int(10) NOT NULL AUTO_INCREMENT;
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
